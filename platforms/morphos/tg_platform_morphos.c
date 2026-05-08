5738a024e35b0839242b3d4e72530328ad5c6894
Michele Dipace <michele.dipace@kaffeine.net>
2026-05-08 08:08:29 +0200

    Make MorphOS TLS optional

--- platforms/morphos/tg_platform_morphos.c
+++ platforms/morphos/tg_platform_morphos.c
@@ -11,8 +11,15 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#ifndef TG_ENABLE_TLS
+#define TG_ENABLE_TLS 0
+#endif
+
+#if TG_ENABLE_TLS
 #include <openssl/err.h>
 #include <openssl/ssl.h>
+#endif
 
 #include "tg_platform.h"
 
@@ -139,6 +146,8 @@ void tg_platform_tcp_close(tg_net_connection *connection)
         close((int)connection->platform_handle);
     }
 }
+
+#if TG_ENABLE_TLS
 
 static void tg_platform_set_ssl_error(char *error_buffer, unsigned long error_buffer_size)
 {
@@ -282,3 +291,60 @@ void tg_platform_tls_close(tg_tls_connection *connection)
     }
     tg_net_close(&connection->tcp);
 }
+
+#else
+
+tg_tls_status tg_platform_tls_connect(tg_tls_connection *connection, const char *host,
+                                      const char *port, tg_net_status *net_status,
+                                      char *error_buffer, unsigned long error_buffer_size)
+{
+    (void)connection;
+    (void)host;
+    (void)port;
+    if (net_status != 0) {
+        *net_status = TG_NET_OK;
+    }
+    if (error_buffer != 0 && error_buffer_size > 0) {
+        error_buffer[0] = '\0';
+    }
+    return TG_TLS_UNSUPPORTED;
+}
+
+tg_tls_status tg_platform_tls_send(tg_tls_connection *connection, const void *data,
+                                   unsigned long byte_count, unsigned long *bytes_sent,
+                                   char *error_buffer, unsigned long error_buffer_size)
+{
+    (void)connection;
+    (void)data;
+    (void)byte_count;
+    if (bytes_sent != 0) {
+        *bytes_sent = 0;
+    }
+    if (error_buffer != 0 && error_buffer_size > 0) {
+        error_buffer[0] = '\0';
+    }
+    return TG_TLS_UNSUPPORTED;
+}
+
+tg_tls_status tg_platform_tls_recv(tg_tls_connection *connection, void *buffer,
+                                   unsigned long buffer_size, unsigned long *bytes_received,
+                                   char *error_buffer, unsigned long error_buffer_size)
+{
+    (void)connection;
+    (void)buffer;
+    (void)buffer_size;
+    if (bytes_received != 0) {
+        *bytes_received = 0;
+    }
+    if (error_buffer != 0 && error_buffer_size > 0) {
+        error_buffer[0] = '\0';
+    }
+    return TG_TLS_UNSUPPORTED;
+}
+
+void tg_platform_tls_close(tg_tls_connection *connection)
+{
+    (void)connection;
+}
+
+#endif
