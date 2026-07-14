07881c5f14f08393639722c838ac3da55ee03976
Michele Dipace <michele.dipace@kaffeine.net>
2026-07-14 19:13:30 +0200

    0.0.6 release: stamp the version date

--- include/tg_version.h
+++ include/tg_version.h
@@ -12,6 +12,6 @@
 
 #define TG_VERSION "0.0.6"
 /* Release date for the Amiga $VER tag (dd.mm.yyyy) -- bump WITH the version. */
-#define TG_VERSION_DATE "05.07.2026"
+#define TG_VERSION_DATE "14.07.2026"
 
 #endif
